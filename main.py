0b97511f66530c9ff4d4f81372833497b0d5a485
Camilo Hernandez <hgcamilo94@gmail.com>
2026-05-12 13:13:31 -0500

    Se creo estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,27 @@
+gastos = []
+
+while True:
+
+    print("\n====== GESTOR DE GASTOS ======")
+    print("1. Registrar gasto")
+    print("2. Mostrar total de gastos")
+    print("3. Buscar gastos por placa")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        pass
+
+    elif opcion == "2":
+        pass
+
+    elif opcion == "3":
+        pass
+
+    elif opcion == "4":
+        print("Saliendo del programa...")
+        break
+
+    else:
+        print("Opción inválida")
